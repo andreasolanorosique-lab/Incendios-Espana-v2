09b6bf518edc47f59a0c2cf9d7e25365c6240221
andreasolanorosique-lab <andrea.solano.rosique@gmail.com>
2026-07-26 21:56:05 +0200

    Update generar_kml.py

--- generar_kml.py
+++ generar_kml.py
@@ -472,7 +472,22 @@ resultado = buscar_effis(lat, lon)
 
 print(resultado)
 exit()
+# ===========================================
+# PRUEBA DE CONSULTA A EFFIS
+# ===========================================
 
+grupo = grupos[0]
+
+lat = sum(f["lat"] for f in grupo) / len(grupo)
+lon = sum(f["lon"] for f in grupo) / len(grupo)
+
+print(f"Consultando EFFIS para: {lat}, {lon}")
+
+resultado = buscar_effis(lat, lon)
+
+print(resultado)
+
+exit()
 # Elimina focos aislados muy débiles
 grupos = [
     grupo
